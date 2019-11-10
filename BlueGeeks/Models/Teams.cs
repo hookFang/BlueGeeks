3c6ecb5de6bb091bda2c0ac6ae60393813ba0414
Michael Valadao-Martins <48562183+MikeVMane@users.noreply.github.com>
2019-11-09 20:43:29 -0500

    Teams model (#6)
    * I made the Teams Model for us
    
    * added the constructor for teams model

---
+++ BlueGeeks/Models/Teams.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueGeeks.Models
+{
+    public class Teams
+    {
+        public Teams() { }
+        public Teams(int Team_Id, String Team_Name, String Team_Mascot, String Conference, short Wins, short Loses, short Ties, short Win_Streak)
+        {
+            this.Team_Id = Team_Id;
+            this.Team_Name = Team_Name;
+            this.Team_Mascot = Team_Mascot;
+            this.Conference = Conference;
+            this.Wins = Wins;
+            this.Loses = Loses;
+            this.Ties = Ties;
+            this.Win_Streak = Win_Streak;
+        }
+
+        [Key]
+        public virtual int Team_Id { get; set; }
+        [Required]
+        public virtual String Team_Name { get; set; }
+        [Required]
+        public virtual String Team_Mascot { get; set; }
+        [Required]
+        public virtual String Conference { get; set; }
+        public virtual short Wins { get; set; }
+        public virtual short Loses { get; set; }
+        public virtual short Ties { get; set; }
+        public virtual short Win_Streak { get; set; }
+
+    }
+}
