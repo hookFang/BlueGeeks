a32fbcb749e34db730bf3b2b594040893a9e6413
Michael VM <michaelvaladaomartins@gmail.com>
2019-11-09 20:24:27 -0500

    added the constructor for teams model

--- BlueGeeks/Models/Teams.cs
+++ BlueGeeks/Models/Teams.cs
@@ -8,6 +8,19 @@ namespace BlueGeeks.Models
 {
     public class Teams
     {
+        public Teams() { }
+        public Teams(int Team_Id, String Team_Name, String Team_Mascot, String Conference, short Wins, short Loses, short Ties, short Win_Streak)
+        {
+            this.Team_Id = Team_Id;
+            this.Team_Name = Team_Name;
+            this.Team_Mascot = Team_Mascot;
+            this.Conference = Conference;
+            this.Wins = Wins;
+            this.Loses = Loses;
+            this.Ties = Ties;
+            this.Win_Streak = Win_Streak;
+        }
+
         [Key]
         public virtual int Team_Id { get; set; }
         [Required]
