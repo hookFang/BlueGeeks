0d3e0ec53a9d07ba612d176c0d58aa158787f260
Vraj <https://github.com/patelvraj810/Vraj-Patel.git>
2019-11-12 15:13:01 -0500

    created three models.

---
+++ BlueGeeks/Models/Coaches.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueGeeks.Models
+{
+    public class Coaches
+    {
+        
+        public Coaches() { }
+        public Coaches(int Coaches_Id, String FirstName, String LastName, String Title )
+        {
+            this.Coaches_Id = Coaches_Id;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Title = Title;
+
+        }
+
+        [Key]
+        public virtual int Coaches_Id { get; set; }
+        [Required]
+        public virtual String FirstName { get; set; }
+        [Required]
+        public virtual String LastName { get; set; }
+        [Required]
+        public virtual String Title { get; set; }
+        public virtual int Team_Id { get; set; }
+        public virtual Teams Team { get; set; }
+    }
+}
+
