b2e74599b06236d123af70ad54a33bb103602e88
Prabhat Batra <Pabhatbatra2000@gmail.com>
2019-11-12 15:39:49 -0500

    I have created the PlayerStatistics.cs model

---
+++ BlueGeeks/Models/PlayerStatistics.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueGeeks.Models
+{
+    public class PlayerStatistics
+    {
+        public class Teams
+        {
+            public Teams() { }
+            public Teams(int Player_Statistics_Id, float FgPercent, float FtPercent, short ThreePointersMade, short PointsMade, short Rebounds, short Assists, short Steals, short Blocks, short TurnOvers)
+            {
+                this.Player_Statistics_Id = Player_Statistics_Id;
+                this.FgPercent = FgPercent;
+                this.FtPercent = FtPercent;
+                this.ThreePointersMade = ThreePointersMade;
+                this.PointsMade = PointsMade;
+                this.Rebounds = Rebounds;
+                this.Assists = Assists;
+                this.Steals = Steals;
+                this.Blocks = Blocks;
+                this.TurnOvers = TurnOvers;
+            }
+
+            [Key]
+            public virtual int Player_Statistics_Id { get; set; }
+            [Required]
+            public virtual float FgPercent { get; set; }
+            [Required]
+            public virtual float FtPercent { get; set; }
+            [Required]
+            public virtual short ThreePointersMade { get; set; }
+            public virtual short PointsMade { get; set; }
+            public virtual short Rebounds { get; set; }
+            public virtual short Assists { get; set; }
+            public virtual short Steals { get; set; }
+            public virtual short Blocks { get; set; }
+            public virtual short TurnOvers { get; set; }
+        }
+    }
+}
