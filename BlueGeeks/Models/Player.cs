24d9478daf64fb2ab7b55d3ffa7e65d377318f6a
Edwin Joseph Christie <48800431+hookFang@users.noreply.github.com>
2019-11-12 14:40:37 -0500

    Added Players model. (#7)
    * Added Players model.
    
    * Added Required to the instance variables

---
+++ BlueGeeks/Models/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueGeeks.Models
+{
+	public class Player
+	{
+		public Player() { }
+
+		public Player(int Player_Id, String FirstName, String LastName, String Position, short JerseyNumber)
+		{
+			this.Player_Id = Player_Id;
+			this.FirstName = FirstName;
+			this.LastName = LastName;
+			this.Position = Position;
+			this.JerseyNumber = JerseyNumber;
+		}
+
+		[Key]
+		public virtual int Player_Id { get; set; }
+		[Required]
+		public virtual String FirstName { get; set; }
+		[Required]
+		public virtual String LastName { get; set; }
+		[Required]
+		public virtual String Position { get; set; }
+		[Required]
+		public virtual short JerseyNumber { get; set; }
+
+		public virtual int TeamId { get; set; }
+		
+		public virtual Teams PlayerTeam { get; set; }
+
+	}
+}
