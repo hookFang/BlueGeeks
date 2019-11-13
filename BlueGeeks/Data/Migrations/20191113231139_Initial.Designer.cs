1947f75a8159f5077573018f4b83e10b37900dcf
Edwin <edwinchristie100@gmail.com>
2019-11-13 18:45:05 -0500

    Added migration initial. Change the create page for player such that it displays the team name instead of id.

--- BlueGeeks/Data/Migrations/20191113231139_Initial.Designer.cs
+++ BlueGeeks/Data/Migrations/20191113231139_Initial.Designer.cs
@@ -4,14 +4,16 @@ using BlueGeeks.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BlueGeeks.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191113231139_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
