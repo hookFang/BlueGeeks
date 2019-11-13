88d509abfb92a85ed50469899a5e3c9a889c5dd2
patelvraj810 <47772732+patelvraj810@users.noreply.github.com>
2019-11-13 10:35:14 -0500

    created three models. (#8)
    * created three models.
    
    * i have made changes

---
+++ BlueGeeks/Models/Matches.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueGeeks.Models
+{
+    public class Matches
+    {
+
+        public Matches() { }
+        public Matches(int Matche_Id, String HomeTeam, String AwayTeam, DateTime MatchDate)
+        {
+            this.Matche_Id = Matche_Id;
+            this.HomeTeam = HomeTeam;
+            this.AwayTeam = AwayTeam;
+            this.MatchDate = MatchDate;
+
+        }
+
+        [Key]
+        public virtual int Matche_Id { get; set; }
+        [Required]
+        public virtual String HomeTeam { get; set; }
+        [Required]
+        public virtual String AwayTeam { get; set; }
+        [Required]
+        public virtual DateTime MatchDate { get; set; }
+        public virtual int AwayTeam_Id { get; set; }
+        public virtual Teams AwayTeam { get; set; }
+        public virtual int Stadium_Id { get; set; }
+        public virtual Stadium HomeTeam { get; set; }
+    }
+}
---
+++ BlueGeeks/Models/Stadium.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueGeeks.Models
+{
+    public class Stadium
+    {
+        public Stadium() { }
+        public Stadium(int Stadium_Id, String StadiumName, String City)
+        {
+            this.Stadium_Id = Stadium_Id;
+            this.StadiumName = StadiumName;
+            this.City = City;
+
+        }
+
+        [Key]
+        public virtual int Stadium_Id { get; set; }
+        [Required]
+        public virtual String StadiumName { get; set; }
+        [Required]
+        public virtual String City { get; set; }
+        public virtual int Team_Id { get; set; }
+        public virtual Teams Team { get; set; }
+    }
+
+
+
+}
+    
+    
+
