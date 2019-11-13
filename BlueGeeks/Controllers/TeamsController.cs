fa97f687b7117b21610dac7174cdce71dd9ddbd3
prabhatbatra12 <45927420+prabhatbatra12@users.noreply.github.com>
2019-11-13 15:22:06 -0500

    added the controllers for stadiums and teams!! (#13)

---
+++ BlueGeeks/Controllers/TeamsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BlueGeeks.Data;
+using BlueGeeks.Models;
+
+namespace BlueGeeks.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TeamsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Teams
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Teams.ToListAsync());
+        }
+
+        // GET: Teams/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var teams = await _context.Teams
+                .FirstOrDefaultAsync(m => m.Team_Id == id);
+            if (teams == null)
+            {
+                return NotFound();
+            }
+
+            return View(teams);
+        }
+
+        // GET: Teams/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Teams/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Team_Id,Team_Name,Team_Mascot,Conference,Wins,Loses,Ties,Win_Streak")] Teams teams)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(teams);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(teams);
+        }
+
+        // GET: Teams/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var teams = await _context.Teams.FindAsync(id);
+            if (teams == null)
+            {
+                return NotFound();
+            }
+            return View(teams);
+        }
+
+        // POST: Teams/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Team_Id,Team_Name,Team_Mascot,Conference,Wins,Loses,Ties,Win_Streak")] Teams teams)
+        {
+            if (id != teams.Team_Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(teams);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TeamsExists(teams.Team_Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(teams);
+        }
+
+        // GET: Teams/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var teams = await _context.Teams
+                .FirstOrDefaultAsync(m => m.Team_Id == id);
+            if (teams == null)
+            {
+                return NotFound();
+            }
+
+            return View(teams);
+        }
+
+        // POST: Teams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var teams = await _context.Teams.FindAsync(id);
+            _context.Teams.Remove(teams);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TeamsExists(int id)
+        {
+            return _context.Teams.Any(e => e.Team_Id == id);
+        }
+    }
+}
