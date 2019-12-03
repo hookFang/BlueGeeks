dc2af2deeddda43522b557627fae7f9319a3e64b
Edwin Joseph Christie <48800431+hookFang@users.noreply.github.com>
2019-12-03 17:31:49 -0500

    Added unit testing for coach, match, stadium, team. (#24)

--- BlueGeeksTest/MatchTest.cs
+++ BlueGeeksTest/MatchTest.cs
@@ -62,5 +62,274 @@ namespace BlueGeeksTest
             var model = Assert.IsAssignableFrom<List<Matches>>(result.ViewData.Model);
             Assert.Equal(1, model.Count());
         }
-    }
+
+
+		/*Test is the delete page for a specific id loads*/
+		[Fact]
+		public async void DeletePageTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new MatchesController(db);
+
+			var match = new Matches { Matche_Id = 2, AwayTeam_Id = 2, MatchDate = DateTime.Now, Stadium_Id = 1 };
+			//Act
+			await c.Create(match);
+
+			var r = await c.Delete(db.Matches.Find(2).Matche_Id);
+
+
+			//Assert
+			var result = Assert.IsType<ViewResult>(r);
+			var model = Assert.IsAssignableFrom<Matches>(result.ViewData.Model);
+			Assert.Equal(db.Matches.Find(2).Matche_Id, model.Matche_Id);
+			Assert.Equal(db.Matches.Find(2).AwayTeam_Id, model.AwayTeam_Id);
+
+		}
+
+		/*Test for null value in ID to delete*/
+		[Fact]
+		public async void DeleteNullPageTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new MatchesController(db);
+
+			//Act
+			var r = await c.Delete(null);
+
+
+			//Assert
+			Assert.IsType<NotFoundResult>(r);
+		}
+
+
+		/*Test for an id that is not present in database*/
+		[Fact]
+		public async void DeleteCoachNotFoundPageTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new MatchesController(db);
+
+			//Act
+			var r = await c.Delete(2);
+
+
+			//Assert
+			Assert.IsType<NotFoundResult>(r);
+		}
+
+		/*Test if deleting an element actually works*/
+		[Fact]
+		public async void DeleteConfirmTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new MatchesController(db);
+
+			var match = new Matches { Matche_Id = 2, AwayTeam_Id = 2, MatchDate = DateTime.Now, Stadium_Id = 1 };
+			//Act
+			var r = await c.Create(match);
+			//Finds the second element stores in a tmp variable
+			var elementToDelete = db.Matches.Find(2);
+
+			await c.DeleteConfirmed(elementToDelete.Matche_Id);
+
+			Assert.DoesNotContain(elementToDelete, db.Matches);
+		}
+
+		/*Test is the details page for a specific id loads*/
+		[Fact]
+		public async void DetailPageTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new MatchesController(db);
+
+			var match = new Matches { Matche_Id = 2, AwayTeam_Id = 1, HomeTeam_Id=1, MatchDate = DateTime.Now, Stadium_Id = 1 };
+			//Act
+			await c.Create(match);
+
+			var r = await c.Details(db.Matches.Find(2).Matche_Id);
+
+
+			//Assert
+			var result = Assert.IsType<ViewResult>(r);
+			var model = Assert.IsAssignableFrom<Matches>(result.ViewData.Model);
+			Assert.Equal(db.Matches.Find(2).Matche_Id, model.Matche_Id);
+			Assert.Equal(db.Matches.Find(2).AwayTeam_Id, model.AwayTeam_Id);
+		}
+
+		/*Test for null value in ID to delete*/
+		[Fact]
+		public async void DetailsNullPageTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new MatchesController(db);
+
+			//Act
+			var r = await c.Details(null);
+
+
+			//Assert
+			Assert.IsType<NotFoundResult>(r);
+		}
+
+
+		/*Test for an id that is not present in database*/
+		[Fact]
+		public async void DetailsCoachNotFoundPageTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new MatchesController(db);
+
+			//Act
+			var r = await c.Details(2);
+
+
+			//Assert
+			Assert.IsType<NotFoundResult>(r);
+		}
+
+		/*Test is the delete page for a specific id loads*/
+		[Fact]
+		public async void EditPageTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new MatchesController(db);
+
+			var match = new Matches { Matche_Id = 2, AwayTeam_Id = 2, MatchDate = DateTime.Now, Stadium_Id = 1 };
+			//Act
+			await c.Create(match);
+
+			var r = await c.Edit(db.Matches.Find(2).Matche_Id);
+
+
+			//Assert
+			var result = Assert.IsType<ViewResult>(r);
+			var model = Assert.IsAssignableFrom<Matches>(result.ViewData.Model);
+			Assert.Equal(db.Matches.Find(2).Matche_Id, model.Matche_Id);
+			Assert.Equal(db.Matches.Find(2).AwayTeam_Id, model.AwayTeam_Id);
+		}
+
+		/*Test for null value in ID to delete*/
+		[Fact]
+		public async void EditNullPageTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new MatchesController(db);
+
+			//Act
+			var r = await c.Edit(null);
+
+
+			//Assert
+			Assert.IsType<NotFoundResult>(r);
+		}
+
+
+		/*Test for an id that is not present in database*/
+		[Fact]
+		public async void EditCoachNotFoundPageTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new MatchesController(db);
+
+			//Act
+			var r = await c.Edit(2);
+
+
+			//Assert
+			Assert.IsType<NotFoundResult>(r);
+		}
+
+		/*Test is theediting actually changes values*/
+		[Fact]
+		public async void EditTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new MatchesController(db);
+			int tempId = 1;
+
+			var match = new Matches { Matche_Id = 2, AwayTeam_Id = 2, MatchDate = DateTime.Now, Stadium_Id = 1 };
+			//Act
+			await c.Create(match);
+
+			match.AwayTeam_Id = tempId;
+			var r = await c.Edit(db.Matches.Find(2).Matche_Id, match);
+
+			//Assert
+			Assert.IsType<RedirectToActionResult>(r);
+			Assert.Equal(db.Matches.Find(2).AwayTeam_Id, tempId);
+		}
+
+		/*Test is wrong id in edit*/
+		[Fact]
+		public async void EditWrongIdTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new MatchesController(db);
+
+			var match = new Matches { Matche_Id = 2, AwayTeam_Id = 2, MatchDate = DateTime.Now, Stadium_Id = 1 };
+			//Act
+			await c.Create(match);
+
+			var r = await c.Edit(db.Matches.Find(1).Matche_Id, match);
+
+			//Assert
+			Assert.IsType<NotFoundResult>(r);
+		}
+
+		/*Test for model state not valid in edit*/
+		[Fact]
+		public async void EditModelStateNotValidTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new MatchesController(db);
+
+			var match = new Matches { Matche_Id = 2, MatchDate = DateTime.Now, Stadium_Id = 1 };
+
+
+			//Act
+			await c.Create(match);
+
+			c.ModelState.AddModelError("AwayTeam_Id", "Required");
+
+			var r = await c.Edit(db.Matches.Find(2).Matche_Id, match);
+
+			//Assert
+			var result = Assert.IsType<ViewResult>(r);
+			var model = Assert.IsAssignableFrom<Matches>(result.ViewData.Model);
+			Assert.Equal(match, model);
+		}
+
+		/*Test if Create Button works*/
+		[Fact]
+		public async void CreateButtonTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new MatchesController(db);
+
+			var match = new Matches { Matche_Id = 2, AwayTeam_Id = 2, MatchDate = DateTime.Now, Stadium_Id = 1 };
+
+			//Act
+			await c.Create(match);
+
+			var r = c.Create();
+
+			//Assert
+			Assert.IsType<ViewResult>(r);
+
+		}
+	}
 }
--- BlueGeeksTest/TeamTest.cs
+++ BlueGeeksTest/TeamTest.cs
@@ -58,5 +58,274 @@ namespace BlueGeeksTest
             var model = Assert.IsAssignableFrom<List<Teams>>(result.ViewData.Model);
             Assert.Equal(1, model.Count());
         }
-    }
+
+
+		/*Test is the delete page for a specific id loads*/
+		[Fact]
+		public async void DeletePageTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new TeamsController(db);
+
+			var teams = new Teams { Team_Name = "Everett Otters", Team_Mascot = "Otter", Team_Id = 2, Conference = "Eastern", Wins = 0, Loses = 0, Ties = 0, Win_Streak = 0 };
+			//Act
+			await c.Create(teams);
+
+			var r = await c.Delete(db.Teams.Find(2).Team_Id);
+
+
+			//Assert
+			var result = Assert.IsType<ViewResult>(r);
+			var model = Assert.IsAssignableFrom<Teams>(result.ViewData.Model);
+			Assert.Equal(db.Teams.Find(2).Team_Id, model.Team_Id);
+			Assert.Equal(db.Teams.Find(2).Team_Name, model.Team_Name);
+
+		}
+
+		/*Test for null value in ID to delete*/
+		[Fact]
+		public async void DeleteNullPageTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new TeamsController(db);
+
+			//Act
+			var r = await c.Delete(null);
+
+
+			//Assert
+			Assert.IsType<NotFoundResult>(r);
+		}
+
+
+		/*Test for an id that is not present in database*/
+		[Fact]
+		public async void DeleteCoachNotFoundPageTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new TeamsController(db);
+
+			//Act
+			var r = await c.Delete(2);
+
+
+			//Assert
+			Assert.IsType<NotFoundResult>(r);
+		}
+
+		/*Test if deleting an element actually works*/
+		[Fact]
+		public async void DeleteConfirmTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new TeamsController(db);
+
+			var teams = new Teams { Team_Name = "Everett Otters", Team_Mascot = "Otter", Team_Id = 2, Conference = "Eastern", Wins = 0, Loses = 0, Ties = 0, Win_Streak = 0 };
+			//Act
+			var r = await c.Create(teams);
+			//Finds the second element stores in a tmp variable
+			var elementToDelete = db.Teams.Find(2);
+
+			await c.DeleteConfirmed(elementToDelete.Team_Id);
+
+			Assert.DoesNotContain(elementToDelete, db.Teams);
+		}
+
+		/*Test is the details page for a specific id loads*/
+		[Fact]
+		public async void DetailPageTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new TeamsController(db);
+
+			var teams = new Teams { Team_Name = "Everett Otters", Team_Mascot = "Otter", Team_Id = 2, Conference = "Eastern", Wins = 0, Loses = 0, Ties = 0, Win_Streak = 0 };
+			//Act
+			await c.Create(teams);
+
+			var r = await c.Details(db.Teams.Find(2).Team_Id);
+
+
+			//Assert
+			var result = Assert.IsType<ViewResult>(r);
+			var model = Assert.IsAssignableFrom<Teams>(result.ViewData.Model);
+			Assert.Equal(db.Teams.Find(2).Team_Id, model.Team_Id);
+			Assert.Equal(db.Teams.Find(2).Team_Name, model.Team_Name);
+		}
+
+		/*Test for null value in ID to delete*/
+		[Fact]
+		public async void DetailsNullPageTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new TeamsController(db);
+
+			//Act
+			var r = await c.Details(null);
+
+
+			//Assert
+			Assert.IsType<NotFoundResult>(r);
+		}
+
+
+		/*Test for an id that is not present in database*/
+		[Fact]
+		public async void DetailsCoachNotFoundPageTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new TeamsController(db);
+
+			//Act
+			var r = await c.Details(2);
+
+
+			//Assert
+			Assert.IsType<NotFoundResult>(r);
+		}
+
+		/*Test is the delete page for a specific id loads*/
+		[Fact]
+		public async void EditPageTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new TeamsController(db);
+
+			var teams = new Teams { Team_Name = "Everett Otters", Team_Mascot = "Otter", Team_Id = 2, Conference = "Eastern", Wins = 0, Loses = 0, Ties = 0, Win_Streak = 0 };
+			//Act
+			await c.Create(teams);
+
+			var r = await c.Edit(db.Teams.Find(2).Team_Id);
+
+
+			//Assert
+			var result = Assert.IsType<ViewResult>(r);
+			var model = Assert.IsAssignableFrom<Teams>(result.ViewData.Model);
+			Assert.Equal(db.Teams.Find(2).Team_Id, model.Team_Id);
+			Assert.Equal(db.Teams.Find(2).Team_Name, model.Team_Name);
+		}
+
+		/*Test for null value in ID to delete*/
+		[Fact]
+		public async void EditNullPageTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new TeamsController(db);
+
+			//Act
+			var r = await c.Edit(null);
+
+
+			//Assert
+			Assert.IsType<NotFoundResult>(r);
+		}
+
+
+		/*Test for an id that is not present in database*/
+		[Fact]
+		public async void EditCoachNotFoundPageTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new TeamsController(db);
+
+			//Act
+			var r = await c.Edit(2);
+
+
+			//Assert
+			Assert.IsType<NotFoundResult>(r);
+		}
+
+		/*Test is theediting actually changes values*/
+		[Fact]
+		public async void EditTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new TeamsController(db);
+			string tempName = "North York";
+
+			var teams = new Teams { Team_Name = "Everett Otters", Team_Mascot = "Otter", Team_Id = 2, Conference = "Eastern", Wins = 0, Loses = 0, Ties = 0, Win_Streak = 0 };
+			//Act
+			await c.Create(teams);
+
+			teams.Team_Name = tempName;
+			var r = await c.Edit(db.Teams.Find(2).Team_Id, teams);
+
+			//Assert
+			Assert.IsType<RedirectToActionResult>(r);
+			Assert.Equal(db.Teams.Find(2).Team_Name, tempName);
+		}
+
+		/*Test is wrong id in edit*/
+		[Fact]
+		public async void EditWrongIdTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new TeamsController(db);
+
+			var teams = new Teams { Team_Name = "Everett Otters", Team_Mascot = "Otter", Team_Id = 2, Conference = "Eastern", Wins = 0, Loses = 0, Ties = 0, Win_Streak = 0 };
+			//Act
+			await c.Create(teams);
+
+			var r = await c.Edit(db.Teams.Find(1).Team_Id, teams);
+
+			//Assert
+			Assert.IsType<NotFoundResult>(r);
+		}
+
+		/*Test for model state not valid in edit*/
+		[Fact]
+		public async void EditModelStateNotValidTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new TeamsController(db);
+
+			var teams = new Teams {Team_Mascot = "Otter", Team_Id = 2, Conference = "Eastern", Wins = 0, Loses = 0, Ties = 0, Win_Streak = 0 };
+
+
+			//Act
+			await c.Create(teams);
+
+			c.ModelState.AddModelError("Team_Name", "Required");
+
+			var r = await c.Edit(db.Teams.Find(2).Team_Id, teams);
+
+			//Assert
+			var result = Assert.IsType<ViewResult>(r);
+			var model = Assert.IsAssignableFrom<Teams>(result.ViewData.Model);
+			Assert.Equal(teams, model);
+		}
+
+		/*Test if Create Button works*/
+		[Fact]
+		public async void CreateButtonTest()
+		{
+			//Arrange           
+			var db = MockDb.CreateMockDb();
+			var c = new TeamsController(db);
+
+			var teams = new Teams { Team_Name = "Everett Otters", Team_Mascot = "Otter", Team_Id = 2, Conference = "Eastern", Wins = 0, Loses = 0, Ties = 0, Win_Streak = 0 };
+
+			//Act
+			await c.Create(teams);
+
+			var r = c.Create();
+
+			//Assert
+			Assert.IsType<ViewResult>(r);
+
+		}
+	}
 }
